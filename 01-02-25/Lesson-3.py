d13f3e45b8d736d9401d2f8de4c69b5cd8a970f2
RAkifev <royache@mail.ru>
2025-02-01 15:58:13 +0200

    # Преобразование типов

---
+++ 01-02-25/Lesson-3.py
@@ -0,0 +1,17 @@
+# Преобразование типов
+var_int = 2 # float(), str(), bool()
+var_float = 2.4 # int(), str(), bool()
+var_str = 'Text' # int(), float(), list(), tuple(), set(), bool()
+var_list= ['1', 'hello'] # str(), tuple(), set(), bool()
+var_tuple = ('hello','1') # str(),, list,(), set(), bool()
+var_set = {4,5,6,7,8,6,4,3,2,3,4,5} # str(), list(), bool()
+var_dict = {'Name':'Anton','Age': 17} # str(), list(), tuple(), set()
+var_bool_1= True # int(), float(), str()
+
+
+
+
+
+
+
+
