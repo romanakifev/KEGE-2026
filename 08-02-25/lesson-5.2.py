82838a60d1642c31a9310e66fc9631313da50b37
RAkifev <royache@mail.ru>
2025-02-08 15:45:59 +0200

    Задача на проверку баллов

---
+++ 08-02-25/lesson-5.2.py
@@ -0,0 +1,17 @@
+num = int(input())
+if 80 <= num <= 100:
+    print('5')
+elif 60  <= num <= 79:
+    print('4')
+elif 40 <= num <= 59:
+    print('3')
+elif num < 40:
+    print('пересдача')
+else:
+    print('недопустимое значение')
+
+
+
+
+
+
